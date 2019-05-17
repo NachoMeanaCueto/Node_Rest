579bfb5ea52d092a2f5cd651b7b9191951244224
Nacho Meana <Nacho Meana@SNGOVD100>
2019-05-17 13:08:06 +0200

    config  ConnectionString

--- config/config.js
+++ config/config.js
@@ -1,13 +1,9 @@
  //set enviroment port
  process.env.PORT = process.env.PORT || 8080;
 
- process.env.NODE_ENV = process.env.NODE_ENV || "dev";
+ process.env.ConnectionString =   process.env.ConnectionString  || 'mongodb://localhost:27017/coffeDb';
 
- process.env.ConnectionString = 'mongodb://localhost:27017/coffeDb';
 
- if(process.env.NODE_ENV != 'dev'){
-    process.env.ConnectionString = 'mongodb+srv://sa:as@cluster0-iuoju.mongodb.net/coffeDb';
- }
  
 
   
